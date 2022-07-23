543d47f4d49aca5d8aebf7373f83d8205d50b74f
SanketJadhav7d3 <lonewolf7d3@gmail.com>
2022-07-23 19:35:53 +0530

    Push stack

--- Stack/stack_linked_list.c
+++ Stack/stack_linked_list.c
@@ -7,6 +7,8 @@ struct Node {
     struct Node* next;
 };
 
+struct Node* top;
+
 struct Node* init_node(int i) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
 
@@ -21,6 +23,7 @@ void push(struct Node* root, int elem) {
     if (root == NULL) {
         struct Node* root = (struct Node*)malloc(sizeof(struct Node));
         root->elem = elem;
+        top = root;
         return;
     }
 
@@ -31,12 +34,50 @@ void push(struct Node* root, int elem) {
     root->next = (struct Node*)malloc(sizeof(struct Node));
     root->next->elem = elem;
     root->next->next = NULL;
+
+    top = root->next;
 }
 
-void pull(struct Node* root) {
+void print_stack(struct Node* root) {
+    if (root != NULL) {
+        printf("Stack elements: \n");
+        do {
+            printf("%d\n", root->elem);
+            root = root->next;
+        } while (root->next != NULL);
+    } else {
+        printf("No elements in stack\n");
+    }
+}
 
+void pop(struct Node* root) {
+    if (root->next == NULL) {
+        // there is only one element in the list
+        root = NULL;
+        return;
+    }
+
+    // to the last second element
+    while (root->next->next != NULL) {
+        root = root->next;
+    }
+
+    // deallocate memory
+    free(root->next);
+
+    top = root;
+    top->next = NULL;
 }
 
 int main() {
+    struct Node* root = init_node(34);
 
+    push(root, 50);
+    push(root, 50);
+    push(root, 50);
+    push(root, 50);
+
+    print_stack(root);
+
+    return 0;
 }
