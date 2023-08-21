aa3b9f892311693d025bb84f36c9aa0b2cf16573
Sanket Jadhav <93857526+SanketJadhav7d3@users.noreply.github.com>
2023-08-21 20:12:05 +0530

    Update fibonacci.py

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -1,17 +1,20 @@
 
+
 def fib(n):
+    global calls
+    calls += 1
     if n <= 2: return 1
     return fib(n - 1) + fib(n - 2)
 
-
 def optimized_fib(n, memo = {}):
+    global calls
+    calls += 1
     if n in memo: return memo[n]
 
     if n <= 2: return 1
 
-    memo[n] = fib(n - 1) + fib(n - 2)
+    memo[n] = optimized_fib(n - 1, memo) + optimized_fib(n - 2, memo)
 
     return memo[n]
 
 
-print(optimized_fib(100))
